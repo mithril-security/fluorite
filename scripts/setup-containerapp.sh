cc3afd0f0cef4d4a90b65a68791ef363f7831a80
Ivan Valentini <ivan@hcompany.ai>
2026-01-26 14:16:32 +0000

    CI: Paralellize some operation to avoid azure auth timeout and add prints

--- scripts/setup-containerapp.sh
+++ scripts/setup-containerapp.sh
@@ -8,27 +8,32 @@ TXT_RECORD_NAME="asuid.$A_RECORD_NAME"
 DOMAIN_NAME="$A_RECORD_NAME.$ZONE_NAME"
 
 # 1. Set minium/maxium number of replicas to 1 so it's always running
+echo "Setting min-max replicas for $CONTAINER_APP"
 az containerapp update \
     -n $CONTAINER_APP \
     -g $RESOURCE_GROUP \
     --cpu 0.5 \
     --memory 1.0Gi \
     --min-replicas 1 \
-    --max-replicas 1
+    --max-replicas 1 &
 
 # 2. If you're configuring an apex domain, get the IP address of your Container Apps environment.
+echo "Getting public IP for $CONTAINER_APP"
 IP=$(az containerapp env show \
     -n $CONTAINER_APP_ENV \
     -g $RESOURCE_GROUP \
     --query "properties.staticIp" \
-    -o tsv)
+    -o tsv) & 
 
 # 3. Get the domain verification code.
+echo "Getting domain verification id for $CONTAINER_APP"
 DOMAIN_VERIFICATION_ID=$(az containerapp show \
                         -n $CONTAINER_APP \
                         -g $RESOURCE_GROUP \
                         -o tsv \
-                        --query "properties.customDomainVerificationId")
+                        --query "properties.customDomainVerificationId") &
+
+wait
 
 # 4. Using the DNS provider that is hosting your domain, create DNS records based on the record type 
 # you selected using the values shown in the Domain validation section. The records point the 
@@ -38,42 +43,34 @@ DOMAIN_VERIFICATION_ID=$(az containerapp show \
 #     A 	@ 	The IP address of your Container Apps environment.
 #     TXT 	asuid 	The domain verification code.
 
-# 5. Remove old TXT record
-az network dns record-set txt delete \
-    -g $RESOURCE_GROUP \
-    -n $TXT_RECORD_NAME \
-    --zone-name $ZONE_NAME \
-    --yes
-
-# 6. Add the TXT record for verification
+# 5. Add the TXT record for verification
+echo "Adding TXT record $TXT_RECORD_NAME"
 az network dns record-set txt add-record \
     -g $RESOURCE_GROUP \
     --zone-name $ZONE_NAME \
     --record-set-name $TXT_RECORD_NAME \
-    --value $DOMAIN_VERIFICATION_ID
+    --value $DOMAIN_VERIFICATION_ID & 
 
-# 7. Remove old A record
-az network dns record-set a delete \
-    -g $RESOURCE_GROUP \
-    -n $A_RECORD_NAME \
-    --zone-name $ZONE_NAME \
-    --yes
-
-# 8. Add the A record for the Container APP
+# 6. Add the A record for the Container APP
+echo "Adding A record $A_RECORD_NAME"
 az network dns record-set a add-record \
     -g $RESOURCE_GROUP \
     --zone-name $ZONE_NAME \
     --record-set-name $A_RECORD_NAME \
     --ipv4-address $IP \
-    --ttl 3600
+    --ttl 3600 & 
 
-# 9. Add the domain to your container app.
+wait
+
+# 7. Add the domain to your container app.
+echo "Adding domain $DOMAIN_NAME to $CONTAINER_APP"
 az containerapp hostname add \
     --hostname $DOMAIN_NAME \
     -g $RESOURCE_GROUP \
     -n $CONTAINER_APP
 
-# 10. Configure the managed certificate and bind the domain to your container app.
+# 8. Configure the managed certificate and bind the domain to your container app.
+echo "Configuring hostname binding for $CONTAINER_APP"
 az containerapp hostname bind \
     --hostname $DOMAIN_NAME \
     -g $RESOURCE_GROUP \
